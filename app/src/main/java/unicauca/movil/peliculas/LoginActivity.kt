480e8e60c946b9fa4f52473f76f0b24d9472fb65
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-11-15 15:22:19 -0500

    Login updated

--- app/src/main/java/unicauca/movil/peliculas/LoginActivity.kt
+++ app/src/main/java/unicauca/movil/peliculas/LoginActivity.kt
@@ -1,12 +1,26 @@
 package unicauca.movil.peliculas
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.startActivity
+import unicauca.movil.peliculas.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding:ActivityLoginBinding = DataBindingUtil
+                .setContentView(this, R.layout.activity_login)
+
+        binding.handler = this
+    }
+
+    fun login(){
+        startActivity<MainActivity>()
+    }
+
+    fun signin(){
+
     }
 }
